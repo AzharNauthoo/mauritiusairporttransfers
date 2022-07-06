d8a71bb919d81eade19f16991f34b39fb0977e72
MrCryptOogly <103628125+MrCryptOogly@users.noreply.github.com>
2022-07-06 11:25:13 +0400

    Update checklogin.php

--- mauritiusairporttransfers/checklogin.php
+++ mauritiusairporttransfers/checklogin.php
@@ -1,18 +1,19 @@
 <?php
-
+session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 include("connection.php");
 
 $sql = "SELECT username, password 
-FROM adminLogin
+FROM adminLogin //table name : adminLogin
 WHERE username = '$username' AND password = '$password'";
 
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0)
 {
+	$_SESSION['username'] = $username;
 	header("Location:adminpage.php");
 }
 
