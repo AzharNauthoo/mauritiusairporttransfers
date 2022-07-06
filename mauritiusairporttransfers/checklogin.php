b3a3cac33bea99b9c8ea194148399275a7d3d1ee
MrCryptOogly <103628125+MrCryptOogly@users.noreply.github.com>
2022-07-06 11:00:55 +0400

    Create checklogin.php

---
+++ mauritiusairporttransfers/checklogin.php
@@ -0,0 +1,26 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+include("connection.php");
+
+$sql = "SELECT username, password 
+FROM login
+WHERE username = '$username' AND password = '$password'";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0)
+{
+	header("Location:adminpage.php");
+}
+
+else 
+{
+	echo "Login failed";
+}
+
+$conn = null;
+?>
