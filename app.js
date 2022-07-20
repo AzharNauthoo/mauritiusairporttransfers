4238f4732ccd7fcab16db736d3965043419390ef
AzharNauthoo <61836941+AzharNauthoo@users.noreply.github.com>
2022-07-20 12:04:55 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
 
+
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         items:1,
@@ -14,4 +15,62 @@ $(document).ready(function () {
         autoplayHoverPause: true
 
     });
+
+
 });
+
+
+
+window.addEventListener('load', () => {
+    const params = (new URL(document.location)).searchParams;
+    const number = params.get('number of passengers');
+    const carOption = params.get('carCar');
+    const carOption1 = params.get('suvSuv');
+    const carOption2 = params.get('vanVan');
+
+    
+    if (carOption == "car"){
+    document.getElementById('vehicle_type').value = carOption;
+        
+    }else if (carOption1 == "suv"){
+        document.getElementById('vehicle_type').value = carOption1;
+
+    } else if(carOption2 == "van") {
+        document.getElementById('vehicle_type').value = carOption2;
+ 
+    }else {
+        console.log("error");
+    }
+
+
+    console.log(document.getElementById('vehicle_type').value);
+
+
+    if (number>5) {
+        document.getElementById("car").classList.add("remove");
+        document.getElementById("suv").classList.add("remove");
+
+    } 
+    else if (number>=3 && number <6)
+    {
+        document.getElementById("car").classList.add("remove");
+    }
+})
+
+
+function returnCar() {
+   var vehicle =  document.getElementById("car_car").value;
+   document.getElementById('vehicle_type').value = vehicle;
+
+
+}
+
+function returnSuv() {
+    var vehicle =  document.getElementById("suv_suv").value;
+    document.getElementById('vehicle_type').value = vehicle;
+ }
+
+ function returnVan() {
+    var vehicle =  document.getElementById("van_van").value;
+    document.getElementById('vehicle_type').value = vehicle;
+ }
