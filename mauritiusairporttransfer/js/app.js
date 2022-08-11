a2a3ce9a467d92ac1b32d356917ea7d10ccc9675
AzharNauthoo <61836941+AzharNauthoo@users.noreply.github.com>
2022-08-11 15:55:58 +0400

    Add files via upload

---
+++ mauritiusairporttransfer/js/app.js
@@ -0,0 +1,82 @@
+
+// javascript for navigation bar effect on scroll
+window.addEventListener("scroll",function(){
+  var header=document.querySelector("header");
+  header.classList.toggle('sticky',window.scrollY > 0);
+});
+
+// javascript for responsive navigation sidebar menu
+var menu=document.querySelector('.menu');
+var menuBtn=document.querySelector('.menu-btn');
+var closeBtn=document.querySelector('.close-btn');
+menuBtn.addEventListener("click",() => {
+ menu.classList.add('active');
+});
+closeBtn.addEventListener("click",()=>{
+  menu.classList.remove('active');
+});
+
+
+ 
+
+
+window.addEventListener('load', () => {
+  const params = (new URL(document.location)).searchParams;
+  const number = params.get('number of passengers');
+  const type = params.get('transferType');
+  const location = params.get('dDestination');
+  document.getElementById('_num-pass').value = number;
+  document.getElementById('_tt-type').value = type;
+  document.getElementById('_d-destination').value = location;
+
+  document.getElementById('hotel1').innerHTML=type;
+  document.getElementById('hotel-name1').innerHTML=location;
+  document.getElementById('hotel2').innerHTML=type;
+  document.getElementById('hotel-name2').innerHTML=location;
+  document.getElementById('hotel3').innerHTML=type;
+  document.getElementById('hotel-name3').innerHTML=location;
+
+
+  // localStorage.setItem("TYPE",type);
+  // localStorage.setItem("LOCATION",location);
+  // localStorage.setItem("NUM",number);
+
+  // console.log(LOCATION)
+//  const number = parseInt(localStorage.getItem('NumPax'));
+
+
+
+  if (number>5) {
+      document.getElementById("car").classList.add("remove");
+      document.getElementById("suv").classList.add("remove");
+
+  } 
+  else if (number>=3 && number <6)
+  {
+      document.getElementById("car").classList.add("remove");
+  }
+
+
+});
+
+
+
+
+
+
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+      items:1,
+      loop:true,
+      nav:true,
+      dots:false,
+      autoplay:true,
+      autoplaySpeed:1000,
+      smartSpeed:1500,
+      autoplayHoverPause: true
+
+  });
+
+
+});
---
+++ mauritiusairporttransfer/js/new.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', () => {
+    const newparams = (new URL(document.location)).searchParams;
+    const carOption =  newparams.get('carCar');
+    const carOption1 = newparams.get('suvSuv');
+    const carOption2 = newparams.get('vanVan');
+  
+    
+    if (carOption == "Standard Car"){
+    document.getElementById('selected-vehicle').value = carOption;
+     document.getElementById('discounted-price').value = 35;
+  
+        
+    }else if (carOption1 == "SUV/ Crossover"){
+        document.getElementById('selected-vehicle').value = carOption1;
+    document.getElementById('discounted-price').value = 40;
+  
+  
+    } else if(carOption2 == "10 Seater Van") {
+        document.getElementById('selected-vehicle').value = carOption2;
+     document.getElementById('discounted-price').value = 50;
+        
+  
+    }else {
+        console.log("error");
+    }
+  
+  
+    const nopax = newparams.get('num-pass');
+    const typeTransfer = newparams.get('tt-type');
+    const loca= newparams.get('d-destination');
+
+    
+  
+    document.getElementById('destination').value = loca;
+    document.getElementById('transfer-type').value = typeTransfer;
+    document.getElementById('num-passengers').value = nopax;
+  
+  
+  });
